9f649a9f80cd59452ddabb44b9e05116cc8eb372
aimeric <holosmoss@gmail.com>
2015-10-13 10:03:39 -0400

    test basé sur tuto (manque la main) http://www.heimetli.ch/ffh/simplifiedsudoku.html

---
+++ Lab2/src/Solveur.java
@@ -0,0 +1,74 @@
+
+public class Solveur implements Runnable {
+
+	private Validateur valid;
+	public Solveur(Validateur v) {
+		//
+		this.valid = v;
+	}
+	/** This method is called by the browser to start the applet */
+	   public void start()
+	   {
+	      // This statement will start the method 'run' to in a new thread
+	      (new Thread(this)).start() ;
+	   }
+
+	   /** The active part begins here */
+	   public void run()
+	   {
+	      try
+	      {
+	         // Let the observers see the initial position
+	         //Thread.sleep( 1000 ) ;
+
+	         // Start to solve the puzzle in the left upper corner
+	         solve( 0, 0 ) ;
+	      }
+	      catch( Exception e )
+	      {
+	      }
+	   }
+
+	   /** Recursive function to find a valid number for one single cell */
+	   public void solve( int row, int col ) throws Exception
+	   {
+	      // Throw an exception to stop the process if the puzzle is solved
+	      if( row > 8 )
+	         throw new Exception( "Solution found" ) ;
+
+	      // If the cell is not empty, continue with the next cell
+	      if( valid.getValue(row,col) != 0 )
+	         next( row, col ) ;
+	      else
+	      {
+	         // Find a valid number for the empty cell
+	         for( int num = 1; num < 10; num++ )
+	         {
+	            if( valid.checkRow(row,num) && valid.checkCol(col,num) && valid.checkBox(row,col,num) )
+	            {
+	            	valid.setValue(row,col,num);
+	               //updateView() ;
+
+	               // Let the observer see it
+	               //Thread.sleep( 1000 ) ;
+
+	               // Delegate work on the next cell to a recursive call
+	               next( row, col ) ;
+	            }
+	         }
+
+	         // No valid number was found, clean up and return to caller
+	         valid.setValue(row,col,0);
+	         //updateView() ;
+	      }
+	   }
+
+	   /** Calls solve for the next cell */
+	   public void next( int row, int col ) throws Exception
+	   {
+	      if( col < 8 )
+	         solve( row, col + 1 ) ;
+	      else
+	         solve( row + 1, 0 ) ;
+	   }
+}
---
+++ Lab2/src/Validateur.java
@@ -0,0 +1,49 @@
+
+public class Validateur {
+
+	private int[][] model = new int[9][9] ;
+	public Validateur(int[][] m) {
+		this.model = m;
+	}
+
+	protected int getValue(int row, int col){
+		return model[row][col];
+	}
+	protected void setValue(int row, int col, int val){
+		model[row][col] = val;
+	}
+
+	/** Checks if num is an acceptable value for the given row */
+	protected boolean checkRow( int row, int num ){
+	  for( int col = 0; col < 9; col++ )
+	     if( model[row][col] == num )
+	        return false ;
+	
+	  return true ;
+	}
+   
+	/** Checks if num is an acceptable value for the given column */
+	protected boolean checkCol( int col, int num ){
+	  for( int row = 0; row < 9; row++ )
+	     if( model[row][col] == num )
+	        return false ;
+	
+	  return true ;
+	}
+   
+	/** Checks if num is an acceptable value for the box around row and col */
+	protected boolean checkBox( int row, int col, int num ){
+		row = (row / 3) * 3 ;
+		col = (col / 3) * 3 ;
+		
+		for( int r = 0; r < 3; r++ )
+			for( int c = 0; c < 3; c++ )
+				if( model[row+r][col+c] == num )
+					return false ;
+		
+		return true ;
+	}
+	   
+	   
+	
+}
